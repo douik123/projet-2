dd67e9cdc07c502e52e22a51f598bceaf0c573ab
douik123 <ahmed@predict-a.com>
2022-07-26 12:29:22 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let input=(<input type="text" placeholder="name"/>)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname="awesome" style={{border:'1px',solid:'red'}}>
+<form>
+<label for="name">enter your name :</label>
+{input}
+</form>
+</div>
+<p> enter your html here </p>
+
     </div>
   );
 }
